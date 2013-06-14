80804235737420eadd8495bffc93a643ea760376
tmr232 <tmr232@github>
2013-06-14 20:50:12 +0300

    Now works. Need to add youtu.be support.

--- Whatube/content.js
+++ Whatube/content.js
@@ -3,6 +3,55 @@
 var facebookYoutubePattern = /((https?:\/\/)?(www\.)facebook.com\/l\.php\?u=)?(https?:\/\/)?(www\.)?youtube\.com\/watch\?(.*)/i;
 var youtubeLinkPattern = /(https?:\/\/)?(www\.)?youtube\.com\/watch\?.*/i;
 
+function getYoutubeTitle(url, callback) {
+    var match = facebookYoutubePattern.exec(unescape(url));
+    if (null === match) {
+        return false;
+    }
+
+    var message = match[match.length - 1];
+
+    chrome.runtime.sendMessage(message, callback);
+}
+
+function setYoutubeLinkTitles() {
+    $("a").each(function (index, value) {
+        if (this.title === "") {
+            getYoutubeTitle(this.href, function (title) {
+                value.title = title;
+            });
+        }
+    });
+}
+
+
+setYoutubeLinkTitles();
+//window.setInterval(setYoutubeLinkTitles, 1000);
+
+//$("a").each(function(index, value) {
+//    getYoutubeTitle(this.href, function(title) {
+//        value.title = title;
+//        console.log(title);
+////        console.log(value);
+//    });
+//});
+
+var observer = new MutationSummary({
+  callback: setYoutubeLinkTitles,
+  queries: [{ attribute: 'href' }]
+});
+
+function handleChanges(summaries) {
+    $("a").each(function (index, value) {
+        getYoutubeTitle(this.href, function (title) {
+            value.title = title;
+            console.log(title);
+            console.log(value);
+        })
+    });
+}
+
+
 function setLinkTitles() {
 
     // Get all links in the page
@@ -36,14 +85,14 @@ function setLinkTitles() {
 }
 
 
-setLinkTitles();
-
-var observer = new MutationSummary({
-  callback: handleChanges,
-  queries: [{ attribute: 'href' }]
-});
-
-function handleChanges(summaries) {
-    setLinkTitles();
-    console.log("yay");
-}
+//setLinkTitles();
+//
+//var observer = new MutationSummary({
+//  callback: handleChanges,
+//  queries: [{ attribute: 'href' }]
+//});
+//
+//function handleChanges(summaries) {
+//    setLinkTitles();
+//    console.log("yay");
+//}

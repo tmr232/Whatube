293d9aef0ba7861e46b19dec08edaaad35a7bc12
tmr232 <tmr232@github>
2013-06-17 22:44:07 +0300

    Added vimeo api call

--- Whatube/background.js
+++ Whatube/background.js
@@ -1,18 +1,15 @@
-//function getPageTitle(pageUrl, callback) {
-//    var xhr = new XMLHttpRequest();
-//    xhr.onload = function () {
-//        console.log(this.responseXML.title);
-//        callback(this.responseXML.title);
-//    };
-//    xhr.open("GET", pageUrl);
-//    xhr.responseType = "document";
-//    xhr.send();
-//}
-
 function getPageTitle(videoId, callback) {
     var apiUrl = "http://gdata.youtube.com/feeds/api/videos/" + videoId + "?v=2&alt=json";
-    $.getJSON(apiUrl, function(data) {
+    $.getJSON(apiUrl, function (data) {
         var title = data.entry.title.$t;
+        callback(title);
+    });
+}
+
+function getVimeoPageTitle(videoId, callback) {
+    var apiUrl = "http://vimeo.com/api/v2/video/" + videoId + ".json";
+    $.getJSON(apiUrl, function (data) {
+        var title = data[0].title;
         callback(title);
     });
 }
@@ -20,9 +17,6 @@ function getPageTitle(videoId, callback) {
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        console.log(request);
-//        var url = "https://www.youtube.com/watch?v=" + request;
-//        getPageTitle(url, sendResponse);
         getPageTitle(request, sendResponse);
         return true;
     });
